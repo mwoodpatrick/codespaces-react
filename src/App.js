27c88b7056b89d6bf834ce0622bb1f260d20c7c1
Mark Wood-Patrick <mwoodpatrick@gmail.com>
2023-01-11 13:42:18 +0000

    Change page info

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React and have a great Wednesday!
           </a>
         </p>
       </header>
